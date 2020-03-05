aa6e7355df845ea00699e2356c3f61faec574b78
Lukas Hirsch <hirschlukasa@gmail.com>
2020-03-05 16:12:13 -0600

    linked output to html and deleted old garbage

--- script.js
+++ script.js
@@ -11,127 +11,14 @@ var upperTest = true;
 var specTest = true;
 var numberTest = true;
 
-//variable for holding all possible character options
+//string variable for holding all possible character options
 //must be "" for concat to work properly
 var passwordOptions = "";
+//array for holding all characters
 var passwordArray = [];
-
-
+//blank variable for final password
 var password = "";
 
-
-
-
-// var lowerArray = [];
-// var upperArray = [];
-// var specialArray = [];
-// var numberArray = [];
-
-// //for loop adds individual characters from alphabet variable to an array
-// for(i=0; i < letterList.length; i++){
-//   lowerArray.push(letterList.charAt(i).toLowerCase());
-// }
-
-// for(i=0; i < letterList.length; i++){
-//   upperArray.push(letterList.charAt(i).toUpperCase());
-// }
-
-// for(i=0; i < specList.length; i++){
-//   specialArray.push(specList.charAt(i).toUpperCase());
-// }
-
-// for(i=0; i < numberList.length; i++){
-//   numberArray.push(numberList.charAt(i).toUpperCase());
-// }
-
-
-// var passwordOptions = "";
-// var lowerCheck = document.querySelector("#lowerCheck");
-
-// lowerCheck.addEventListener('change', function(){
-//   if(lowerCheck.checked ===true){
-//     passwordOptions = passwordOptions.concat(letterList.toLowerCase());
-//     console.log(" Lower ", passwordOptions);
-//   }
-//   else{
-//     console.log("nope ",passwordOptions);
-//   }
-// });
-
-
-
-// function generatePassword(){
-  
-//logic for concatinating string of possible characters
-
-
-
-
-
-
-// var lowerCheck = document.querySelector("#lowerCheck");
-
-// lowerCheck.addEventListener('change', function(){
-//   if(lowerCheck.checked === true){
-//     passwordOptions = passwordOptions.concat(letterList.toLowerCase());
-//     console.log(" Lower ", passwordOptions);
-//   }
-//   else{
-//     passwordOptions = passwordOptions.replace(/[^A-Z\.]+/g, "");
-//     console.log("nope ",passwordOptions);
-//   }
-// });
-
-
-// var upperCheck = document.querySelector("#upperCheck");
-// // if(upperTest){
-// //   passwordOptions = passwordOptions.concat(letterList.toUpperCase());
-// //   console.log(" Upper ", passwordOptions);
-// // }
-// upperCheck.addEventListener('change', function(){
-//   if(upperCheck.checked ===true){
-//     passwordOptions = passwordOptions.concat(letterList.toUpperCase());
-//     console.log(" Upper ", passwordOptions);
-//   }
-//   else{
-//     //probably wont need this
-//     // passwordOptions = passwordOptions.replace(/[^a-z\.]+/g, "");
-//     // console.log("nope ",passwordOptions);
-//   }
-// });
-
-// var specCheck = document.querySelector("#specCheck");
-
-// // if(specTest ===true){
-// //   passwordOptions = passwordOptions.concat(specList);
-// //   console.log(" special " , passwordOptions);
-// // }
-// specCheck.addEventListener('change', function(){
-//   if(specCheck.checked ===true){
-//     passwordOptions = passwordOptions.concat(specList);
-//     console.log(" special " , passwordOptions);
-//   }
-//   else{
-
-//     console.log("nope ",passwordOptions);
-//   }
-// });
-
-// var numberCheck = document.querySelector("#numberCheck");
-// // if(numberTest ===true){
-// //   passwordOptions = passwordOptions.concat(numberList);
-// //   console.log(" number " , passwordOptions);
-// // }
-// numberCheck.addEventListener('change', function(){
-//   if(numberCheck.checked ===true){
-//     passwordOptions = passwordOptions.concat(numberList);
-//     console.log(" number " , passwordOptions);
-//   }
-//   else{
-//     console.log("nope ",passwordOptions);
-//   }
-// });
-// }
 
 var lowerCheck = document.querySelector("#lowerCheck");
 var upperCheck = document.querySelector("#upperCheck");
@@ -213,15 +100,26 @@ generateBtn.addEventListener("click", function(){
   }
   
 
-  
+  console.log(password);
 
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+  //resets password and password option string
+  password = "";
+  passwordOptions="";
+  passwordArray = [];
   //if no checked, tell user
   
 
 
-  //clear checkboxes
+  
 });
 
+
+//reset button
+
+//clear checkboxes
 
 
 
